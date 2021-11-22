3b3efe6efa5dd64c6ef710e024c66ee33ba05c76
pai <69026987+paij0se@users.noreply.github.com>
2021-11-21 19:29:34 -0500

    :eyeglasses:

--- src/client/client.ts
+++ src/client/client.ts
@@ -1,14 +1,37 @@
 import * as peo from "https://denopkg.com/iamnathanj/cursor@v2.2.0/mod.ts";
 import * as ink from "https://deno.land/x/ink@1.3/mod.ts";
 import { emptyDir } from "https://deno.land/std/fs/mod.ts";
-import { showSongs } from "./utils/showSongs.ts";
+import { showSongs, fileNames } from "./utils/showSongs.ts";
 // clear screen
 await peo.clearScreen();
-
+console.log(
+  (await Deno.readTextFile("welcome.txt")) + "\n",
+  "welcome to ymp3cli\n",
+  "Type <ctrl + c> to exit.\n",
+  "version 0.0.1\n"
+);
 while (true) {
-  showSongs();
+  console.log(ink.colorize(`<magenta>avaiable songs:\n${showSongs}</magenta>`));
 
   const input = prompt(ink.colorize("<green>youtube url:</green>"));
+  console.log(ink.colorize(`<yellow>downloading: ${input}</yellow>`));
+  const play = prompt(
+    ink.colorize("<blue>play a random song? (y:yes, n:no):</blue>")
+  );
+  if (play === "y") {
+    const randomSong = fileNames[Math.floor(Math.random() * fileNames.length)];
+    console.log(ink.colorize(`<yellow>playing: ${randomSong} </yellow>`));
+    // play a random song from the music folder
+    const process = Deno.run({
+      cmd: ["mpg321", "music/" + randomSong],
+      stdout: "piped",
+      stderr: "piped",
+    });
+
+    const output = await process.output();
+    const outStr = new TextDecoder().decode(output);
+    console.log(outStr);
+  }
   const del = prompt(
     ink.colorize("<red>delete previous songs? (y:yes, n:no):</red>")
   );
@@ -16,7 +39,6 @@ while (true) {
     // delete previous songs
     emptyDir("./music");
   }
-  console.log(ink.colorize("<blue>avaiable songs:</blue>"));
   const rawResponse = await fetch("http://127.0.0.1:8000/download", {
     method: "POST",
     headers: {
--- src/client/utils/showSongs.ts
+++ src/client/utils/showSongs.ts
@@ -1,15 +1,9 @@
-import * as ink from "https://deno.land/x/ink@1.3/mod.ts";
+export const fileNames: string[] = [];
 
-export async function showSongs() {
-  const cmd = Deno.run({
-    cmd: ["ls", "-a", "music"],
-    stderr: "piped",
-    stdout: "piped",
-    stdin: "null",
-  });
-
-  const rawOutput = await cmd.output();
-  cmd.close();
-  const output: string = new TextDecoder().decode(rawOutput); // here decode de output hex => text
-  console.log(ink.colorize(`<magenta>${output}</magenta>`));
+for await (const dirEntry of Deno.readDir("music")) {
+  if (dirEntry.isFile) {
+    fileNames.push(dirEntry.name);
+  }
 }
+
+export const showSongs: string = fileNames.join("\n");
