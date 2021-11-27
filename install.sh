9b6c2224346b41a663c80d8c779a769c2aba5792
pai <69026987+paij0se@users.noreply.github.com>
2021-11-27 03:06:34 -0500

    :+1:

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 echo downloading dependencies
 cd src ; go get . ; cd ..
+go mod download github.com/ELPanaJose/pairat
+go mod download github.com/manifoldco/promptui
+go get github.com/manifoldco/promptui@v0.9.0
 echo dependencies installed, now run ./ymp3cli to start
