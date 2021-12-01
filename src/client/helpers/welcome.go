936d68cf4232b35f4ddbb55ab0259ac415778b67
pai <69026987+paij0se@users.noreply.github.com>
2021-12-01 09:52:05 -0500

    0.0.7

--- src/client/helpers/welcome.go
+++ src/client/helpers/welcome.go
@@ -20,6 +20,6 @@ func Welcome() {
 	`
 	fmt.Println(ymp3cli)
 	fmt.Printf("\nwelcome to ymp3cli!\n\n")
-	fmt.Printf("Version 0.0.6\n")
+	fmt.Printf("Version 0.0.7\n")
 	fmt.Printf("\nType <ctrl + c> to exit.\n\n")
 }
