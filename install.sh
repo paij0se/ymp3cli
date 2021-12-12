e955be72879ee19d8e4e880add6ebefb5f8df618
paijose <hachikopolarcito321@gmail.com>
2021-12-11 20:54:00 -0500

    :neutral_face:

--- install.sh
+++ install.sh
@@ -7,6 +7,6 @@ cd src ; go get . ; cd ..
 go mod download github.com/ELPanaJose/pairat
 go mod download github.com/manifoldco/promptui
 go get github.com/manifoldco/promptui@v0.9.0
-echo dependencies installed, now run <ymp3cli>
+echo dependencies installed, now run ymp3cli
 go build -o ymp3cli src/main.go
 sudo mv ymp3cli /bin
